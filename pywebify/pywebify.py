36323fad9b89e46108f370398ebe6aedd692c89f
endangeredoxen <steve.nicholes@gmail.com>
2015-11-21 21:07:23 -0700

    fixed bad path

--- pywebify/pywebify.py
+++ pywebify/pywebify.py
@@ -32,7 +32,7 @@ def copy_config(dir):
     filename = 'pywebify.ini'
     if not os.path.exists(dir):
         os.makedirs(dir)
-    shutil.copyfile('pywebify\config.ini', osjoin(dir, filename))
+    shutil.copyfile('config.ini', osjoin(dir, filename))
 
 
 class PyWebify():
