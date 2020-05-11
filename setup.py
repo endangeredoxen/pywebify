fd49c223905f538d51d7213105341479a49b1057
Steve Nicholes <steve.nicholes@gmail.com>
2020-05-11 09:26:36 -0600

    setup files for pypi

--- setup.py
+++ setup.py
@@ -22,10 +22,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.3',
+    version='0.2.4',
 
     description='Browser-based html/image file report builder',
-    long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/endangeredoxen/pywebify',
@@ -39,24 +38,24 @@ setup(
     license='GPLv3',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 5',
+    # classifiers=[
+    #     # How mature is this project? Common values are
+    #     #   3 - Alpha
+    #     #   4 - Beta
+    #     #   5 - Production/Stable
+    #     'Development Status :: 5',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Engineers/Scientists',
-        'Topic :: Data Analysis',
+    #     # Indicate who your project is intended for
+    #     'Intended Audience :: Engineers/Scientists',
+    #     'Topic :: Data Analysis',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: GPL v3 License',
+    #     # Pick your license as you wish (should match "license" above)
+    #     'License :: GPL v3 License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
-    ],
+    #     # Specify the Python versions you support here. In particular, ensure
+    #     # that you indicate whether you support Python 2, Python 3 or both.
+    #     'Programming Language :: Python :: 3.6',
+    # ],
 
     # What does your project relate to?
     keywords=['data', 'web report'],
