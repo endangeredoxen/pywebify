d8975ce78b86fc2dda5bd2ef5f72c6f8f2989097
Steve Nicholes <steve.nicholes@gmail.com>
2023-03-22 11:44:05 -0600

    update jquery version

--- src/pywebify/js/div_toggle.js
+++ src/pywebify/js/div_toggle.js
@@ -6,28 +6,25 @@
 
 function div_toggle() {
     var origWidth = $('#sidebar').width();
+    var status = 0;
     $('#toggle').html('<<');
-    $('#toggle').toggle(function(){
-    $('#sidebar').css({'overflow': 'hidden'});
-    $('#sidebar li').css({'visibility': 'hidden'});
-    $('#sidebar').animate({width:0});
-    $('#viewer').animate({left:0});
-    $('#toggle').html('>>');
-    },function(){
-    $('#sidebar').css({'overflow': 'auto'});
-    $('#sidebar li').css({'visibility': 'visible'});
-    $('#sidebar').animate({width:origWidth});
-    $('#toggle').html('<<');
-    });
-    
-    // position the toggle at the bottom and account for a reduced viewer div height
-//    var h1 = $('#viewer').height();
-//    $('#viewer').height('100%');
-//    var h2 = $('#viewer').height();
-//    var h3 = h2-h1;
-//
-//    $('#toggle').css({'bottom':h3+'px'});
-//    $('#viewer').height(h1);
+    $('#toggle').on('click', function() {
+        if (status == 0) {
+            $('#sidebar').css({'overflow': 'hidden'});
+            $('#sidebar li').css({'visibility': 'hidden'});
+            $('#sidebar').animate({width:0});
+            $('#viewer').animate({left:0});
+            $('#toggle').html('>>');
+            status = 1;
+        } else {
+            $('#sidebar').css({'overflow': 'auto'});
+            $('#sidebar li').css({'visibility': 'visible'});
+            $('#sidebar').animate({width:origWidth});
+            $('#toggle').html('<<');
+            status = 0;
+        }
+    }
+    );
 };
 
 $(document).ready( function() {
